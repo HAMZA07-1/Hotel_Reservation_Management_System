9676f0f1bd0d1ee02bcd758db5686fb50017778e
DanSweet734 <daniel.sweet.734@my.csun.edu>
2025-09-30 16:52:14 -0700

    Add files via upload

---
+++ UnitTestFile.py
@@ -0,0 +1,107 @@
+import unittest
+from hotel_models import Room
+from hotel_models import Guest
+from hotel_models import Reservation
+from datetime import datetime
+
+#Test order: Room, Guest, Reservation
+#Room Test
+class RoomTestCase(unittest.TestCase):
+
+    #Intializaiton Test
+    def test_room_initialization(self):
+        room = Room(101, "Non-Smoking", 4, 110, True)
+        self.assertEqual(room.room_number, 101)
+        self.assertEqual(room.room_type, "Non-Smoking")
+        self.assertEqual(room.capacity, 4)
+        self.assertEqual(room.price, 110)
+        self.assertTrue(room.is_available)
+
+    #Set Methods Test
+    def test_room_update_methods(self):
+        room = Room(101, "Non-Smoking", 4, 110, True)
+
+        room.set_number(101)
+        self.assertEqual(room.room_number, 101)
+
+        room.set_type("Smoking")
+        self.assertEqual(room.room_type, "Smoking")
+
+        room.set_capacity(10)
+        self.assertEqual(room.capacity, 10)
+
+        room.set_price(1)
+        self.assertEqual(room.price, 1)
+
+        room.set_available(False)
+        self.assertEqual(room.is_available, False)
+
+#Guest Tests
+class GuestTestcase(unittest.TestCase):
+
+    #Initialization Test
+    def test_guest_initialization(self):
+        guest = Guest("Daniel", "Sweet", 90137, "daniel.sweet.734@my.csun.edu", "818-123-4567", "18111 Nordhoff Street Northridge, CA 91330")
+        self.assertEqual(guest.first_name, "Daniel")
+        self.assertEqual(guest.last_name, "Sweet")
+        self.assertEqual(guest.email, "daniel.sweet.734@my.csun.edu")
+        self.assertEqual(guest.phone, "818-123-4567")
+        self.assertEqual(guest.address, "18111 Nordhoff Street Northridge, CA 91330")
+
+    #Set Methods Test
+    def test_guest_update_methods(self):
+        guest = Guest("Daniel", "Sweet", 90137, "daniel.sweet.734@my.csun.edu", "818-123-4567","18111 Nordhoff Street Northridge, CA 91330")
+
+        guest.set_first_name("George")
+        self.assertEqual(guest.first_name, "George")
+        guest.set_last_name("Smith")
+        self.assertEqual(guest.last_name, "Smith")
+        guest.set_guest_id(21948)
+        self.assertEqual(guest.guest_id, 21948)
+        guest.set_email("sweet.daniel.437@my.csun.edu")
+        self.assertEqual(guest.email, "sweet.daniel.437@my.csun.edu")
+        guest.set_phone("818-987-6543")
+        self.assertEqual(guest.phone, "818-987-6543")
+        guest.set_address("308 Westwood Plz, Los Angeles, CA 90095-8355")
+        self.assertEqual(guest.address, "308 Westwood Plz, Los Angeles, CA 90095-8355")
+
+
+#Reservation Tests
+class ReservationTest(unittest.TestCase):
+
+    #Initialization Test
+    def test_reservation_initialization(self):
+        room = Room(101, "Non-Smoking", 4, 110, True)
+        guest = Guest("Daniel", "Sweet", 90137, "daniel.sweet.734@my.csun.edu", "818-123-4567", "18111 Nordhoff Street Northridge, CA 91330")
+        reservation = Reservation(1234, guest, room, datetime(2025,9,30), datetime(2025,10, 2))
+        self.assertEqual(reservation.order_id, 1234)
+        self.assertEqual(reservation.guest, guest)
+        self.assertEqual(reservation.room, room)
+        self.assertEqual(reservation.check_in_date, datetime(2025,9,30))
+        self.assertEqual(reservation.check_out_date, datetime(2025,10,2))
+
+    #Update Methods Test
+    def test_reservation_update_methods(self):
+        room = Room(101, "Non-Smoking", 4, 110, True)
+        guest = Guest("Daniel", "Sweet", 90137, "daniel.sweet.734@my.csun.edu", "818-123-4567","18111 Nordhoff Street Northridge, CA 91330")
+        reservation = Reservation(1234, guest, room, datetime(2025, 9, 30), datetime(2025, 10, 2))
+
+        reservation.set_order_id(4321)
+        self.assertEqual(reservation.order_id, 4321)
+        better_guest = Guest("Keano", "Aquino", 91853, "good.worker@hotmail.com", "408-249-5398", "1083 South Winchester Boulevard, San Jose CA 95128")
+        reservation.set_guest(better_guest)
+        self.assertEqual(reservation.guest, better_guest)
+        better_room = Room(202, "Smoking", 12, 500, False)
+        reservation.set_room(better_room)
+        self.assertEqual(reservation.room, better_room)
+        reservation.set_check_in_date(datetime(2025, 10, 31))
+        self.assertEqual(reservation.check_in_date, datetime(2025, 10, 31))
+        reservation.set_check_out_date(datetime(2025, 11, 21))
+        self.assertEqual(reservation.check_out_date, datetime(2025, 11, 21))
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- hotel_models.py
+++ hotel_models.py
@@ -1,10 +1,79 @@
-# D-1: Initial Room Class Definition
-class Room:
-    """Respresents a Hotel room. """
+"""
+Hotel Management Application
+Room, Guest, and Reservation Classes
+"""
+from datetime import datetime
+import sqlite3
 
-    def __init__(self, room_number, room_type, price_per_night, is_available =True):
+class Room:
+    def __init__(self, room_number, room_type, capacity, price, is_available):
         self.room_number = room_number
         self.room_type = room_type
-        self.price_per_night = price_per_night
+        self.capacity = capacity
+        self.price = price
         self.is_available = is_available
-        pass
+
+    #Set method
+    def set_number(self, room_number):
+        self.room_number = room_number
+    def set_type(self, room_type):
+        self.room_type = room_type
+    def set_capacity(self, capacity):
+        self.capacity = capacity
+    def set_price(self, price):
+        self.price = price
+    def set_available(self, is_available):
+        self.is_available = is_available
+
+
+class Reservation:
+    def __init__(self, order_id, guest, room, check_in_date, check_out_date):
+        self.order_id = order_id
+        self.guest = guest
+        self.room = room
+        self.check_in_date = check_in_date
+        self.check_out_date = check_out_date
+
+    #Set methods
+    def set_order_id(self, order_id):
+        self.order_id = order_id
+    def set_guest(self, guest):
+        self.guest = guest
+    def set_room(self, room):
+        self.room = room
+    def set_check_in_date(self, check_in_date):
+        self.check_in_date = check_in_date
+    def set_check_out_date(self, check_out_date):
+        self.check_out_date = check_out_date
+
+
+class Guest:
+    def __init__(self, first_name, last_name, guest_id, email, phone, address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.guest_id = guest_id
+        self.email = email
+        self.phone = phone
+        self.address = address
+
+
+    #Set methods
+    def set_first_name(self, first_name):
+        self.first_name = first_name
+    def set_last_name(self, last_name):
+        self.last_name = last_name
+    def set_guest_id(self, guest_id):
+        self.guest_id = guest_id
+    def set_email(self, email):
+        self.email = email
+    def set_phone(self, phone):
+        self.phone = phone
+    def set_address(self, address):
+        self.address = address
+
+
+
+class DatabaseManager:
+    def __init__(self, room_db, guest_db):
+        self.room_db = room_db
+        self.guest_db = guest_db
