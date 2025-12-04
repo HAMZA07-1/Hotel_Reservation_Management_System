3084e29168bced9c47895f037fcb59f07bdff20f
DanSweet734 <daniel.sweet.734@my.csun.edu>
2025-12-04 00:12:26 -0800

    Update main.py

--- Single Screen Prototype/main.py
+++ Single Screen Prototype/main.py
@@ -63,7 +63,7 @@ class HotelApp(tk.Tk):
             frame.grid(row=0, column=0, sticky="nsew")
 
         #First Frame shown on program start
-        self.show_frame("main_menu")
+        self.show_frame("login_screen")
 
     #def to show new screen
     def show_frame(self, name):
